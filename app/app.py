b1f5de6bc1bb9f5831288be2ff1aad9b38ebf9f2
Tristan Cusi <cusitristan@gmail.com>
2021-11-13 18:10:24 -0800

    added ability to try reconnect to mysql after 30 sec

--- app/app.py
+++ app/app.py
@@ -1,13 +1,25 @@
 import mysql.connector
 import time
-
-mydb = mysql.connector.connect(
-  host="mysql",
-  user="root",
-  password="root",
-  database="courses"
-)
-
+try:
+  mydb = mysql.connector.connect(
+    host="mysql",
+    user="root",
+    password="root",
+    database="courses"
+  )
+except:
+  print("Could not connect to MySQ, sleeping 30 sec then trying again")
+  time.sleep(30)
+  try:
+    mydb = mysql.connector.connect(
+      host="mysql",
+      user="root",
+      password="root",
+      database="courses"
+    )
+  except:
+    print("Could not connect to MySQL after 30sec")
+    
 mycursor = mydb.cursor()
 
 mycursor.execute("SELECT * FROM teachers;")
@@ -16,5 +28,5 @@ for row in mycursor:
     print(row)
 
 while 1:
-	print("looping")
-	time.sleep(5)
+  print("looping")
+  time.sleep(5)
